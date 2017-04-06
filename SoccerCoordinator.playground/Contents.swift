5d36e48a680107911f253c83655cc7b8cbacd4ee
Jose Portuondo-Dember <jose@helo.local>
2017-04-06 19:18:38 -0400

    Player dictionaries

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,7 +1,35 @@
-//: Playground - noun: a place where people can play
+//Dictionaries for individual players
 
-import UIKit
+let joeSmith: [String: Any] = ["fullName": "Joe Smith", "height": 42, "hasExperience": true, "guardianName(s)": "Jim and Jan Smith"]
 
-var str = "Hello, playground"
+let jillTanner: [String: Any] = ["fullName": "Jill Tanner", "height": 36, "hasExperience": true, "guardianName(s)": "Clara Tanner"]
 
-//testing this out
+let billBonn: [String: Any] = ["fullName": "Bill Bon", "height": 43, "hasExperience": true, "guardianName(s)": "Sara and Jenny Bon"]
+let evaGordon: [String: Any] = ["fullName": "Eva Gordon", "height": 45, "hasExperience": false, "guardianName(s)": "Wendy and Mike Gordon"]
+
+let mattGill: [String: Any] = ["fullName": "Matt Gill", "height": 40, "hasExperience": false, "guardianName(s)": "Charles and Sylvia Gill"]
+let kimmyStein: [String: Any] = ["fullName": "Kimmy Stein", "height": 41, "hasExperience": false, "guardianName(s)": "Bill and Hillary Stein"]
+
+let sammyAdams: [String: Any] = ["fullName": "Sammy Adams", "height": 45, "hasExperience": false, "guardianName(s)": "Jeff Adams"]
+
+let karlSaygan: [String: Any] = ["fullName": "Karl Saygan", "height": 42, "hasExperience": true, "guardianName(s)": "Heather Bledsoe"]
+
+let suzaneGreenberg: [String: Any] = ["fullName": "Suzane Greenberg", "height": 44, "hasExperience": true, "guardianName(s)": "Henrietta Dumas"]
+
+let salDali: [String: Any] = ["fullName": "Sal Dali", "height": 41, "hasExperience": false, "guardianName(s)": "Gala Dali"]
+
+let joeKavalier: [String: Any] = ["fullName": "Joe Kavalier", "height": 39, "hasExperience": false, "guardianName(s)": "Sam and Elaine Kavalier"]
+
+let benFinkelstein: [String: Any] = ["fullName": "Ben Finkelstein", "height": 44, "hasExperience": false, "guardianName(s)": "Aaron and Jill Finkestein"]
+
+let diegoSoto: [String: Any] = ["fullName": "Diego Soto", "height": 41, "hasExperience": true, "guardianName(s)": "Robin and Sarika Soto"]
+
+let chloeAlaska: [String: Any] = ["fullName": "Chloe Alaska", "height": 47, "hasExperience": false, "guardianName(s)": "David and Jamie Alaska"]
+
+let arnoldWillis: [String: Any] = ["fullName": "Arnold Willis", "height": 43, "hasExperience": false, "guardianName(s)": "Claire Willis"]
+
+let phillipHelm: [String: Any] = ["fullName": "Phillip Helm", "height": 44, "hasExperience": true, "guardianName(s)": "Thomas Helm and Eva Jones"]
+
+let lesClay: [String: Any] = ["fullName": "Les Clay", "height": 42, "hasExperience": true, "guardianName(s)": "Wynona Brown"]
+
+let herschelKrustofski: [String: Any] = ["fullName": "Herschel Krustofski", "height": 45, "hasExperience": true, "guardianName(s)": "Hyman and Rachel Krustofski"]
